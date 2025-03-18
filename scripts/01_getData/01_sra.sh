e60f94ab9606dd565fe468743920f01e329aa699
Noah Reid <noah.reid@uconn.edu>
2025-03-18 17:16:04 -0400

    data download

---
+++ scripts/01_getData/01_sra.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=sra
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 12
+#SBATCH --mem=15G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=first.last@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+#################################################################
+# Download fastq files from SRA 
+#################################################################
+
+# load software
+module load parallel/20180122
+module load sratoolkit/3.0.1
+
+# The data are from this study:
+
+OUTDIR=../../data/fastq
+    mkdir -p ${OUTDIR}
+METADATA=../../metadata/SraRunTable.txt
+
+ACCLIST=../../metadata/accessionlist.txt
+
+# use parallel to download 2 accessions at a time. 
+cat $ACCLIST | head -n 3 | parallel -j 3 "fasterq-dump -O ${OUTDIR} {}"
+
+# compress the files 
+ls ${OUTDIR}/*fastq | parallel -j 3 gzip
---
+++ scripts/01_getData/02_Li_etal.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=Li_etal
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 2
+#SBATCH --mem=15G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=first.last@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+OUTDIR=../../data/Li_etal_2022/
+mkdir -p ${OUTDIR}
+
+# download data from figshare
+    # Li, Q., Wang, M., Zhang, P. et al. A single-cell transcriptomic atlas tracking the neural basis of division of labour in an ant superorganism. Nat Ecol Evol 6, 1191–1204 (2022). https://doi.org/10.1038/s41559-022-01784-1
+wget -P ${OUTDIR} https://figshare.com/ndownloader/articles/16616353/versions/2
+
+# the zip file is just named '2'
+unzip ${OUTDIR}/2
+
+# remove zipfile
+rm ${OUTDIR}/2
+
+# decompress genomic resources
+for file in ${OUTDIR}/M.pharaonis*gz; do gunzip $file; done
