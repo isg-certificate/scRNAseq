ed01c1375885a2cc32bf6ab74f5bec09e1d10e87
Noah Reid <noah.reid@uconn.edu>
2025-03-20 15:05:26 -0400

    counts

--- scripts/01_getData/02_Li_etal.sh
+++ scripts/01_getData/02_Li_etal.sh
@@ -20,7 +20,12 @@ cd ${OUTDIR}
 
 # download data from figshare
     # Li, Q., Wang, M., Zhang, P. et al. A single-cell transcriptomic atlas tracking the neural basis of division of labour in an ant superorganism. Nat Ecol Evol 6, 1191–1204 (2022). https://doi.org/10.1038/s41559-022-01784-1
+    # https://figshare.com/articles/dataset/Supporting_data_for_A_single-cell_transcriptomic_atlas_tracking_the_neural_basis_of_division_of_labor_in_an_ant_superorganism_/16616353
+
 wget https://figshare.com/ndownloader/articles/16616353/versions/2
+
+# make sure you get this file Mpha.umi.geneMatrix.gz and name it this. 
+wget https://figshare.com/ndownloader/files/35070388
 
 # the zip file is just named '2'
 unzip 2
--- scripts/03_counts/02_starSOLO.sh
+++ scripts/03_counts/02_starSOLO.sh
@@ -27,14 +27,17 @@ FQBARCODE=${INDIR}/SRR19020406_1.fastq.gz
 # read file needs to be first, barcode file second (reverse from our data)
      # usually you want a barcode whitelist, but we don't have one. 
      # --soloCBmatchWLtype 0 means treat every barcode sequence as unique. Will have to obvious failures later. 
-STAR --runThreadN 16 \
-     --genomeDir ${GENOME} \
-     --readFilesIn ${FQREAD} ${FQBARCODE} \
-     --readFilesCommand zcat \
-     --soloType CB_UMI_Simple \
-     --soloCBstart 1 17 \
-     --soloCBlen 10 10 \
-     --soloUMIstart 32 \
-     --soloUMIlen 10 \
-     --soloCBmatchWLtype 0 \
-     --outFileNamePrefix ${OUTDIR}
+STAR \
+    --runThreadN 16 \
+    --genomeDir ${GENOME} \
+    --readFilesIn ${FQREAD} ${FQBARCODE} \
+    --readFilesCommand zcat \
+    --soloType CB_UMI_Complex \
+    --soloBarcodeReadLength 41 \
+    --soloCBposition 0_0_0_10 0_17_0_26 \
+    --soloUMIposition 0_32_0_41 \
+    --soloCBwhitelist None None \
+    --soloCBmatchWLtype Exact \
+    --outFileNamePrefix ${OUTDIR}/SRR19020406_CB_UMI_complex
+
+
